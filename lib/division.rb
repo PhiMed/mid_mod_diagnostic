5d861cd824f1840582ffc94a7064d421d031ab56
Philip Medlin <phmedlin@gmail.com>
2021-09-09 14:32:09 -0600

    Feat add emplyee roster method

--- lib/division.rb
+++ lib/division.rb
@@ -10,4 +10,16 @@ class Division
   def add_departments(department)
     @departments << department
   end
+
+  def depts_with_mult_employees
+    @departments.find_all do |dept|
+      dept.employees.count >= 2
+    end
+  end
+
+  def divisional_employee_roster
+    @departments.map do |dept|
+      dept.employee_names
+    end.flatten
+  end
 end
